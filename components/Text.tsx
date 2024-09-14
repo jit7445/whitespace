5b45e1d75428ff2d57a6673accd0b173483e27bf
jitender_verma <139689987+jit7445@users.noreply.github.com>
2024-09-14 20:14:14 +0530

    Update Text.tsx

--- components/Text.tsx
+++ components/Text.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Textarea } from "@/components/ui/textarea";
-import {  SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+
 interface HomeProps {}
 
 const Text: React.FC<HomeProps> = () => {
@@ -12,59 +12,71 @@ const Text: React.FC<HomeProps> = () => {
   };
 
   const insertWhitespace = () => {
-    // Split the input text by newline characters to get individual paragraphs
+   
     const paragraphs = sentence.split('\n');
-
+    
     const modifiedParagraphs = paragraphs.map((paragraph) => {
-      // Split the paragraph by periods or commas
-      const clauses = paragraph.split(/([.,])/);
-
-      const wordspace = clauses.map((clause, index) => {
-        if (clause !== '.' && clause !== ',') {
-          let words = clause.split(' ').filter(word => word); // Split clause into words
-
-          for (let i = 0; i < 3; i++) {
-            let randomIndex = Math.floor(Math.random() * words.length);
-            let word = words[randomIndex];
-            if (word && word.length > 2) {
-              let randomPosition = 1;
-              words[randomIndex] = word.substring(0,1) + '\u200B' + word.substring(1);
-            }
-          }
-
-          // Join the words back into a clause
-          return words.join(' ') + (index % 2 !== 0 ? clauses[index] : '');
-        }
-
-        return clause;
-      }).join('');
-
-      return wordspace;
+      
+      const boldTextRegex = /\*\*(.*?)\*\*/g;
+      let lastIndex = 0;
+      let result = '';
+  
+     
+      paragraph.replace(boldTextRegex, (match, p1, offset) => {
+       
+        result += insertInvisibleSpaces(paragraph.slice(lastIndex, offset));
+        
+        result += match;
+        lastIndex = offset + match.length;
+        return match;
+      });
+  
+     
+      result += insertInvisibleSpaces(paragraph.slice(lastIndex));
+      
+      return result;
     });
-
+    
     setModifiedParagraphs(modifiedParagraphs);
   };
-
+  
+  const insertInvisibleSpaces = (text: string) => {
+   
+    const clauses = text.split(/([.,])/);
+    
+    return clauses.map((clause, index) => {
+      if (/\s{2,}/.test(clause)) {
+       
+        return clause;
+      }
+  
+      let words = clause.split(' ').filter(word => word); // Split clause into words
+      
+      for (let i = 0; i < 3; i++) {
+        let randomIndex = Math.floor(Math.random() * words.length);
+        let word = words[randomIndex];
+        if (word && word.length > 2) { // Check if word is valid and has length > 2
+          // Randomly place one zero-width space in the word
+          let randomPosition = Math.floor(Math.random() * (word.length - 1)) + 1;
+          words[randomIndex] = word.substring(0, randomPosition) + '\u200B' + word.substring(randomPosition);
+        }
+      }
+      
+  
+      return words.join(' ') + (index % 2 !== 0 ? clauses[index] : '');
+    }).join('');
+  };
+  
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-8 bg-gray-900">
-      <header className='flex justify-center items-center bg-gray-400 rounded-xl'>
-       <div className='flex justify-center items-center  p-2'>
-      <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-            </div>
-      
-      </header>
       <h1 className="text-4xl font-bold text-yellow-400 mb-6">Whitespace Inserter</h1>
       <Textarea
         value={sentence}
         onChange={handleChange}
         placeholder="Enter your paragraphs here..."
         className="border border-gray-700 p-4 mt-2 bg-gray-800 text-white rounded-lg shadow-md text-xl"
-        rows={30} // Adjust the number of rows as needed
+        rows={30}
         cols={40} // Adjust the number of columns as needed
       />
       <button
